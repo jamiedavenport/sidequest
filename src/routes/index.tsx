08346edf6b04d0cbcc0f638482540c73cc717eb3
Jamie Davenport <1329874+jamiedavenport@users.noreply.github.com>
2026-08-29 12:02:50 +0100

    Remove Welcome component and update routing to use new Component in index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,7 @@
 import { createFileRoute } from "@tanstack/react-router";
 
-import { Welcome } from "~/components/welcome.tsrx";
+import { Component } from "./index.tsrx";
 
 export const Route = createFileRoute("/")({
-  component: Welcome,
+  component: Component,
 });
